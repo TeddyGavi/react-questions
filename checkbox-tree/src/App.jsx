a29fe4c8ea40d708e1cb45dcc4660f4f32b2e65a
TeddyGavi <matc.davis@gmail.com>
2023-10-26 09:34:09 -0700

    init checkbox tree

---
+++ checkbox-tree/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import data from './data'
+import { useState } from 'react'
+
+const Main = () => {
+  const [formData, setFormData] = useState({ checkedTree: [] })
+
+  const handleSubmit = () => {
+    console.log(formData)
+  }
+
+  return (
+    <div className='container'>
+      <h1>Mock Form</h1>
+      <input placeholder='Mock Input' className='common' />
+      <input placeholder='Mock Input' className='common' />
+      {/* <CheckedTree /> */}
+      <button onClick={handleSubmit} className='common'>
+        Submit
+      </button>
+    </div>
+  )
+}
+
+export default Main
