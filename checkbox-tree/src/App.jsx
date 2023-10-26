b6543a0fc6d95e7e6f483e0aa133323597d76de9
TeddyGavi <matc.davis@gmail.com>
2023-10-26 10:18:27 -0700

    working on tree building...

--- checkbox-tree/src/App.jsx
+++ checkbox-tree/src/App.jsx
@@ -2,8 +2,45 @@ import './App.css'
 import data from './data'
 import { useState } from 'react'
 
+
+
 const Main = () => {
   const [formData, setFormData] = useState({ checkedTree: [] })
+
+  const buildTree = (response) => {
+    const tree = []
+
+    for (let i = 0; i < response.length; i++) {
+      const parent = {children: []}
+      while (!parent.children.length){
+        parent.id = response[i].id
+        parent.name = response[i].name
+        parent.children.push()
+      }
+
+  //     if (!response[i].parentId) {
+  //       parent.id = response[i].id
+  //       parent.name = response[i].name
+  //       parent.children = []
+  //       tree.push(parent)
+  //     } 
+
+    }
+
+  // console.log(tree)
+  //   for (let i= 0; i < response.length; i++) {
+  //     // for (const {id} of tree) {
+  //       const parent = tree.find((parent) => parent.id === response[i].parentId)
+  //     // }
+  //       if (parent) {
+  //         parent.children.push(response[i])
+  //       }
+  //   }
+
+  console.log(tree)
+  }
+
+  buildTree(data)
 
   const handleSubmit = () => {
     console.log(formData)
