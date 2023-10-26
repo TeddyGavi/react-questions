4a518a7b653fb0ca12a6f98105ed3b9a00cc7c25
TeddyGavi <matc.davis@gmail.com>
2023-10-26 15:42:30 -0700

    tree working, need to do checkbox clicking!

--- checkbox-tree/src/App.jsx
+++ checkbox-tree/src/App.jsx
@@ -1,34 +1,33 @@
-import './App.css'
-import data from './data'
-import { useEffect, useState } from 'react'
-import buildTree from './buildTree'
-import CheckedTree from './CheckedTree'
-
+import "./App.css";
+import data from "./data";
+import { useEffect, useState } from "react";
+import buildTree from "./buildTree";
+import CheckedTree from "./CheckedTree";
 
 const Main = () => {
-  const [formData, setFormData] = useState({ checkedTree: [] })
- const [initData, setInitData] = useState([])
- useEffect(() => {
-   setInitData(buildTree(data))
+  const [formData, setFormData] = useState({ checkedTree: [] });
+  const [initData, setInitData] = useState([]);
 
- },[])
-
+  
+  useEffect(() => {
+    setInitData(buildTree(data));
+  }, []);
 
   const handleSubmit = () => {
-    console.log(formData)
-  }
+    console.log(formData);
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Mock Form</h1>
-      <input placeholder='Mock Input' className='common' />
-      <input placeholder='Mock Input' className='common' />
+      <input placeholder="Mock Input" className="common" />
+      <input placeholder="Mock Input" className="common" />
       <CheckedTree initData={initData} />
-      <button onClick={handleSubmit} className='common'>
+      <button onClick={handleSubmit} className="common">
         Submit
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
--- checkbox-tree/src/CheckedTree.jsx
+++ checkbox-tree/src/CheckedTree.jsx
@@ -1,19 +1,69 @@
+import { useState } from "react";
+
 export default function CheckedTree({ initData }) {
+  const [checked, setChecked] = useState(false);
+
+  const toggleChecked = (id) => {
+    console.log(id)
+    setChecked(prev => !prev)
+  }
+
   return (
     <div className="checked-tree-container">
-      {initData.map(({ id, name, parentId, isChecked }) => {
+      {initData.map((item) => {
         return (
-          <div key={id} className="checkbox-row">
-            &gt;
-            <input  type="checkbox" checked={isChecked} onClick={() => null}></input>
-            <label htmlFor="checkbox">&nbsp;{name}</label>
-          </div>
+          <ItemInCheckbox
+            key={item.id}
+            singleChild={item}
+            checked={checked}
+            toggleChecked={toggleChecked}
+          />
         );
       })}
     </div>
   );
 }
 
+const ItemInCheckbox = ({
+  singleChild: {  name,  isChecked, children }, checked, toggleChecked
+}) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={`checkbox-row-${name.toLowerCase()}`}>
+      <div style={{ display: "flex" }}>
+        <div onClick={() => setOpen((prev) => !prev)}>{open ? "V" : ">"}</div>
+        <input
+          type="checkbox"
+          name={name}
+          checked={checked}
+          onChange={() => toggleChecked()}
+        ></input>
+        <label htmlFor={name}>&nbsp;{name}</label>
+      </div>
+      {open &&
+        children.map((child) => (
+          <div
+            className={`nested-child ${child.name.toLowerCase()}`}
+            key={child.id}
+          >
+            <ItemInCheckbox thisClass="nested" singleChild={child} toggleChecked={toggleChecked} />
+          </div>
+        ))}
+    </div>
+  );
+};
+
 CheckedTree.propTypes = {
   initData: [],
+};
+
+ItemInCheckbox.propTypes = {
+  singleChild: {
+    id: Number,
+    name: String,
+    parentId: Number,
+    isChecked: Boolean,
+    children: Array,
+  },
 };
